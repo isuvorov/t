0d2b4aa0b56efbcd1a9afe54f446b96b793687df
Igor Suvorov <hi@isuvorov.com>
2024-02-08 12:35:50 +0300

    fix: fix

--- src/tx-validate.ts
+++ src/tx-validate.ts
@@ -1,69 +1,11 @@
-export interface Transaction {
-  id: number;
-  orderId: number;
-  amount: number;
-  txType: 'Bet' | 'Win';
-}
+import type { Result, Transaction, UserBalance } from './types';
 
-type UserBalance = number;
-
-type Options = {
-  comparator: (a: Transaction, b: Transaction) => number;
-};
-
-export interface ValidatedTransaction extends Transaction {
-  valid: boolean;
-}
-
-type Result = {
-  transactions: ValidatedTransaction[];
-  balance: UserBalance;
-};
-
-const defaultOptions = {
-  // но что делать с порядком orderId внутри id?
-  comparator: (a: Transaction, b: Transaction) => a.id - b.id,
-};
-
-export function txValidate(
-  transactions: Transaction[],
-  initBalance: UserBalance = 0,
-  options: Options = defaultOptions,
-): Result {
-  const transactionIds = new Set<number>();
-  const validTransactions = new Map<number, boolean>();
-
-  const sortedTransactions = [...transactions].sort(options.comparator);
-
-  let balance = initBalance;
-  for (let i = 0; i < sortedTransactions.length; i++) {
-    const tx = sortedTransactions[i];
-    if (transactionIds.has(tx.id)) {
-      validTransactions.set(i, false);
-      continue;
-    }
-    if (tx.txType === 'Bet') {
-      // Тоесть все таки не уходим в минус? или проверяем но не позволяем? tx.amount >= balance
-      balance -= tx.amount;
-      if (balance < 0) {
-        validTransactions.set(i, false);
-        continue;
-      }
-    } else if (tx.txType === 'Win') {
-      balance += tx.amount;
-    } else {
-      // throw?
-    }
-    validTransactions.set(i, true);
-  }
-
-  // обсуждаемо, нужно ли возвращать в таком формате или "по минимому"
-  // единственный аргумент за такой формат - удобен для покрытия тестов
+export function txValidate(transactions: Transaction[], initBalance: UserBalance = 0): Result {
   return {
-    transactions: sortedTransactions.map((tx, i) => ({
+    transactions: transactions.map((tx, i) => ({
       ...tx,
-      valid: validTransactions.get(i) || false,
+      valid: i % 2 === 0,
     })),
-    balance,
+    balance: initBalance * 2,
   };
 }
